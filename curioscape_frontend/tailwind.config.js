0532ab6a1b3436999a25e25ab9b12a54e1ac2190
Divya <divyareddy0205@gmail.com>
2023-06-08 12:00:53 -0700

    tailwindcss

--- curioscape_frontend/tailwind.config.js
+++ curioscape_frontend/tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: 
+  theme: //customizing the theme of the CSS framework or library
   {
-    extend: 
+    extend: //extending or overriding existing theme configurations
     {
-      margin:{
+      margin://custom margin size
+      {
         320:'320px',
       },
-      width:{
+      width://several custom width sizes with specific pixel values
+      {
         190: '190px',
         275: '275px',
         300: '300px',
@@ -18,7 +20,8 @@ module.exports = {
         880: '880px',
         508: '508px',
       },
-      height: {
+      height: //several custom height sizes with specific pixel values
+      {
         80: '80px',
         340: '340px',
         370: '370px',
@@ -27,31 +30,37 @@ module.exports = {
         600: '600px',
         685: '685px',
         800: '800px',
-        '90vh': '90vh',
+        '90vh': '90vh',//90% of the viewport heigh
       },
-      flex:{
+      flex://custom flex value of 0.7 1 0%, which can be used in CSS flexbox layouts.
+      {
         0.7: '0.7 1 0%',
       },
-      maxHeight:{
+      maxHeight://custom maxHeight value
+      {
         370:'370px',
       },
-      minWidth:{
+      minWidth://several custom minWidth values with specific pixel values
+      {
         210: '210px',
         350: '350px',
         620: '620px',
       },
-      textColor: {
+      textColor: //custom text color values
+      {
         lightGray: '#F1EFEE',
         primary: '#FAFAFA',
         secColor: '#efefef',
         navColor: '#BEBEBE',
       },
-      backgroundColor: {
+      backgroundColor: //custom background color values
+      {
         mainColor: '#FBF8F9',
         secondaryColor: '#F0F0F0',
         blackOverlay: 'rgba(0, 0 ,0 ,0.7)',
       },
-      keyframes: {
+      keyframes: //custom keyframes for CSS animations
+      {
         'slide-in': {
           '0%': {
             '-webkit-transform': 'translateX(-200px)',
@@ -74,11 +83,13 @@ module.exports = {
           },
         },//slide-fwd
       },//keyframes
-      animation: {
+      animation: //custom animation values that reference the defined keyframes
+      {
         'slide-in': 'slide-in 0.5s ease-out',
         'slide-fwd': ' slide-fwd 0.45s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
       },
-      transitionProperty: {
+      transitionProperty: //custom transition property for transitioning the height of an element.
+      {
         height: 'height',
       },
     },//extend
@@ -88,9 +99,10 @@ module.exports = {
       pointer: 'pointer',
     },
   },//theme
-  variants: {
+  variants: //customizing the variants of the CSS framework or library
+  {
     extend: {},
   },
-  plugins: [],
+  plugins: [],//include additional plugins or extensions to enhance the functionality of the CSS framework or library
 }
 
