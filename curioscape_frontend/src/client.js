40c2cab234baf961054cd2973f718d1f5e4d8c35
Divya <divyareddy0205@gmail.com>
2023-06-08 15:00:54 -0700

    client.js

--- curioscape_frontend/src/client.js
+++ curioscape_frontend/src/client.js
@@ -4,6 +4,10 @@ import { createClient } from "@sanity/client";
 //imageUrlBuilder function is used to create an image URL builder instance, which helps in constructing URLs for images stored in your Sanity.io project
 import imageUrlBuilder from "@sanity/image-url";
 
+/*
+- creates a client instance using the createClient function. 
+- The createClient function takes an object with various configuration options as an argument.
+*/
 export const client = createClient({
   projectId : process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset:'production',
