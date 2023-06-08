ad19b35fc1de3d9fb475672197dd30f051d25c1d
Divya <divyareddy0205@gmail.com>
2023-06-08 14:57:12 -0700

    client.js

--- curioscape_frontend/src/client.js
+++ curioscape_frontend/src/client.js
@@ -1,7 +1,7 @@
-import { SanityClient } from "@sanity/cli";
+import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-export const client = sanityClient({
+export const client = createClient({
   projectId : process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset:'production',
   apiVersion:'2023-06-08',
