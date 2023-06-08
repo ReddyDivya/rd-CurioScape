492e854950a99af82d05233a452b6c392095ddfe
Divya <divyareddy0205@gmail.com>
2023-06-08 15:56:26 -0700

    login

--- curioscape_frontend/src/components/Login.jsx
+++ curioscape_frontend/src/components/Login.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
-import GoogleLogin from 'react-google-login';
 import {FcGoogle} from 'react-icons/fc';
+
+/*
+react-google-login=> allows you to integrate Google Sign-In functionality into your React application.
+GoogleLogin component is used to render a Google Sign-In button and handle the authentication process with Google.
+*/
+import GoogleLogin from '@leecheuk/react-google-login';
+
 
 const Login = () => {
     /*
@@ -31,6 +37,22 @@ const Login = () => {
                 {/*displays logo*/}
                 <div className="p-5">
                     <img src={logo} width="130px"/>
+                </div>
+
+                {/*displays google login button*/}
+                <div className="shadow-2xl">
+                    <GoogleLogin
+                        clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+                        render={(renderProps) => (
+                            <button type="button"
+                            className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                            onClick={renderProps.onClick}
+                            disabled={renderProps.disabled}
+                            >
+                                <FcGoogle className="mr-4" /> Sign in with google
+                            </button>
+                        )}
+                    />    
                 </div>
             </div>
         </div>
