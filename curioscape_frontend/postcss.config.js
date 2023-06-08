8143d7cedc5f4338d0955544431ad6171337f9ad
Divya <divyareddy0205@gmail.com>
2023-06-08 12:11:18 -0700

    tailwindcss

--- curioscape_frontend/postcss.config.js
+++ curioscape_frontend/postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   plugins: [
+    //loads the Tailwind CSS plugin and points it to your tailwind.config.js file
     require('tailwindcss')('./path/to/your/tailwind.config.js'),
+    
+    //The second plugin require('autoprefixer') is used to automatically add vendor prefixes to CSS properties for better browser compatibility.
     require('autoprefixer'),
-    // Add any other PostCSS plugins you may need
   ]
 }
