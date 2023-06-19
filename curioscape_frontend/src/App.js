2d597069c777e9bbc8a1cb45a4cacc71d61dcdee
Divya <divyareddy0205@gmail.com>
2023-06-19 12:55:55 -0700

    login

--- curioscape_frontend/src/App.js
+++ curioscape_frontend/src/App.js
@@ -3,22 +3,30 @@ import Login from './components/Login';
 
 const App = () => {
 
-  const handleCallbackResponse = () => {
-    
+  const handleCallbackResponse = (response) => {
+    console.log('response >> '+ response.credentials);
   }//handleCallbackResponse
 
   useEffect(() => {
-    return () => {
-      //global google
-      google.accounts.id.initialize({
-        client_id: "913338086499-9apvhk80qbsc0k226hrv0e83c0knb8q3.apps.googleusercontent.com",
-        callback:handleCallbackResponse
-      })
-    }
+
+    //global google
+    google.accounts.id.initialize({
+      client_id: "913338086499-9apvhk80qbsc0k226hrv0e83c0knb8q3.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton( document.getElementById("signInDiv"),
+    {
+      theme: "outline",
+      size: "large"
+    });
   }, [])
   
   return (
     <div>
+      <div id="signInDiv">
+      
+      </div>
       <Login/>
     </div>
   )
