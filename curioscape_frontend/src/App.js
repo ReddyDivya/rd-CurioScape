0cb5c9dcc138bc91187eabe9082315727b442290
Divya <divyareddy0205@gmail.com>
2023-06-21 15:57:28 -0700

    app-component

--- curioscape_frontend/src/App.js
+++ curioscape_frontend/src/App.js
@@ -6,10 +6,13 @@ import Home from './container/Home';
 
 const App = () => {
   const navigate = useNavigate();
-  
+
   return (
     <GoogleOAuthProvider clientId='913338086499-q1ef425rdmicqv0ssg63i8ni42u884sa.apps.googleusercontent.com'>
-      <Login/>
+      <Routes>
+        <Route path="login" element={<Login/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </GoogleOAuthProvider>
   )
 }
