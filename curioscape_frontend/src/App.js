4794bfcfeb91710e86a25a7c004d281bd99bbd18
Divya <divyareddy0205@gmail.com>
2023-06-21 15:46:56 -0700

    app-component

--- curioscape_frontend/src/App.js
+++ curioscape_frontend/src/App.js
@@ -1,8 +1,11 @@
 import React, {useEffect} from 'react';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import Login from './components/Login';
 import {GoogleOAuthProvider} from '@react-oauth/google';
+import Home from './container/Home';
 
 const App = () => {
+  const navigate = useNavigate();
   
   return (
     <GoogleOAuthProvider clientId='913338086499-q1ef425rdmicqv0ssg63i8ni42u884sa.apps.googleusercontent.com'>
