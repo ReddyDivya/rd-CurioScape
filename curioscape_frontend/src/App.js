7eaac4e0d73af4df820c81e063b7d583d45277e7
Divya <divyareddy0205@gmail.com>
2023-06-21 15:22:11 -0700

    home-component

--- curioscape_frontend/src/App.js
+++ curioscape_frontend/src/App.js
@@ -3,30 +3,11 @@ import Login from './components/Login';
 import {GoogleOAuthProvider} from '@react-oauth/google';
 
 const App = () => {
-
-  // function handleCredentialResponse(response) {
-  //   console.log("Encoded JWT ID token: " + response);
-  // }
-
-  // useEffect(() => {
-
-  //   //global google
-  //   google.accounts.id.initialize({
-  //     client_id: "913338086499-q1ef425rdmicqv0ssg63i8ni42u884sa.apps.googleusercontent.com",
-  //     callback: handleCredentialResponse
-  //   });
-
-  //   google.accounts.id.renderButton(
-  //     document.getElementById("signInDiv"),
-  //     { theme: "outline", size: "large" }  // customization attributes
-  //   );
-  //   google.accounts.id.prompt(); // also display the One Tap dialog
-  // }, [])
   
   return (
-    <GoogleOAuthProvider clientId='913338086499-q1ef425rdmicqv0ssg63i8ni42u884sa.apps.googleusercontent.com'>
+    // <GoogleOAuthProvider clientId='913338086499-q1ef425rdmicqv0ssg63i8ni42u884sa.apps.googleusercontent.com'>
       <Login/>
-    </GoogleOAuthProvider>
+    // </GoogleOAuthProvider>
   )
 }
 
