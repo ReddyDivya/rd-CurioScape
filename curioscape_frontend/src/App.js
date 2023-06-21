00d948bcb948b77d5ace0fab402713f65d9a828d
Divya <divyareddy0205@gmail.com>
2023-06-21 16:39:28 -0700

    app-component

--- curioscape_frontend/src/App.js
+++ curioscape_frontend/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   useEffect(() => {
     //fetching user info from the localstorage
     const User = localStorage.getItem('user') !== 'undefined'
-    ? JSON.parse(localStorage.getItem('user')) 
+    ? localStorage.getItem('user') 
     : localStorage.clear();
 
     //if user info doesn't exists from the localstorage, then navigate to login
