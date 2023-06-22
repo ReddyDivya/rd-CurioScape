b6f9274f8d3737246827f529a23819e305139c36
Divya <divyareddy0205@gmail.com>
2023-06-22 16:54:09 -0700

    sidebar

--- curioscape_frontend/src/components/Sidebar.jsx
+++ curioscape_frontend/src/components/Sidebar.jsx
@@ -19,6 +19,7 @@ const Sidebar = ({closeToggle, user}) => {
   return (
     <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
         <div className="flex flex-col">
+            {/* Displaying logo */}
             <Link to="/"
                 className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
                 onClick={handleCloseSidebar} 
@@ -26,6 +27,8 @@ const Sidebar = ({closeToggle, user}) => {
                 <img src={logo} alt="logo" className="w-full"/>
             </Link>
             <div className="flex flex-col gap-5">
+                
+                {/* Displaying Home icon */}
                 <NavLink to="/"
                 className={({isActive}) => (isActive ? isActiveStyle : isNotActiveStyle)}
                 onClick={handleCloseSidebar}
@@ -33,18 +36,21 @@ const Sidebar = ({closeToggle, user}) => {
                   <RiHomeFill/>
                   Home
                 </NavLink>
+
+                {/* Displaying Book category images and name*/}
                 <h3 className="mt-2 px-5 text-base 2xl:text-xl">Discover categories</h3>
-                {categories.slice(0, categories.length - 1).map((category) => (
-                  <NavLink
-                    to={`/category/${category.name}`}
-                    className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
-                    onClick={handleCloseSidebar}
-                    key={category.name}
-                  >
-                    <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" />
-                    {category.name}
-                  </NavLink>
-                ))}
+                {
+                  categories.slice(0, categories.length - 1).map((category) => ( 
+                    <NavLink to={`/category/${category.name}`}
+                      onClick={handleCloseSidebar}
+                      className={({isActive}) => (isActive ? isActiveStyle : isNotActiveStyle)}
+                      key={category.name}
+                    >
+                      <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" />
+                      {category.name}
+                    </NavLink>  
+                  ))
+                }
             </div>
         </div>
     </div>
