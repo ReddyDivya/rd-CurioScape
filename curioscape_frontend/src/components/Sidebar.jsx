fb5b697006b1192cf8a3c529ad7b18fe9341a832
Divya <divyareddy0205@gmail.com>
2023-06-22 12:24:21 -0700

    sidebar-component

---
+++ curioscape_frontend/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
+
+    </div>
+  )
+}
+
+export default Sidebar
