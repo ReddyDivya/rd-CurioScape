96fa9a1adddf56c069f110b2591b312319f9c1d2
Divya <divyareddy0205@gmail.com>
2023-06-22 12:29:01 -0700

    sidebar-component

--- curioscape_frontend/src/components/Sidebar.jsx
+++ curioscape_frontend/src/components/Sidebar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {NavLink, Link} from 'react-router-dom';
+import logo from '../assets/logo.png';
+import {RiHomeFill} from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
 
-const Sidebar = () => {
+const Sidebar = ({closeToggle, user}) => {
+
+  //close the sidebar
+  const handleCloseSidebar = () => {
+    if(closeToggle)
+        closeToggle(false);
+  };//handleCloseSidebar
+
   return (
     <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
-
+        <div className="flex flex-col">
+            
+        </div>
     </div>
   )
 }
