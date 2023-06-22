8c560cbb9c911462a1a6dcb76ab6422b945b582a
Divya <divyareddy0205@gmail.com>
2023-06-22 12:39:17 -0700

    sidebar-component

--- curioscape_frontend/src/container/Home.jsx
+++ curioscape_frontend/src/container/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Sidebar from '../components/Sidebar';
 
 const Home = () => {
+  const [user, setUser] = useState();
+
+  //fetching user info from the localstorage
+  const userInfo = localStorage.getItem('user') !== 'undefined'
+  ? localStorage.getItem('user') 
+  : localStorage.clear();
+
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
-      Home
+      <div className="hidden md:flex h-screen flex-initial">
+          <Sidebar/>
+      </div>
     </div>
   )
 }
