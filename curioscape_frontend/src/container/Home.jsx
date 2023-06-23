2fe42f88d5a1d31a5a3540b6a10a0a24afb8eb85
Divya <divyareddy0205@gmail.com>
2023-06-23 14:58:45 -0700

    Home

--- curioscape_frontend/src/container/Home.jsx
+++ curioscape_frontend/src/container/Home.jsx
@@ -52,6 +52,12 @@ const Home = () => {
       <div className="flex md:hidden flex-row">
           <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
               <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)}/>
+              <Link to="/">
+                <img src={logo} alt="logo" className="w-28" />
+              </Link>
+              <Link to={`user-profile/${user?._id}`}>
+                <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full" />
+              </Link>
           </div>
       </div>
     </div>
