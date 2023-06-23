81b0dcc95ad994a5f21739066cd89013f0a681f5
Divya <divyareddy0205@gmail.com>
2023-06-23 15:41:34 -0700

    Home

--- curioscape_frontend/src/container/Home.jsx
+++ curioscape_frontend/src/container/Home.jsx
@@ -73,7 +73,7 @@ const Home = () => {
         {toggleSidebar && (
           <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
               <div className="absolute w-full flex justify-end items-center p-2">
-                <AiFillCloseCircle fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
+              <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
               </div>
               <Sidebar closeToggle={setToggleSidebar} user={user && user}/>
           </div>  
