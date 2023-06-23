3184a0ffe060930855690545b0229fc7a5894bcd
Divya <divyareddy0205@gmail.com>
2023-06-23 15:35:19 -0700

    Home

--- curioscape_frontend/src/container/Home.jsx
+++ curioscape_frontend/src/container/Home.jsx
@@ -53,29 +53,31 @@ const Home = () => {
     z-10: sets the z-index of the element to 10. The z-index determines the stacking order of elements on the z-axis. In this case, the element will be positioned above elements with lower z-index values.
     animate-slide-in: applies an animation to the element called "slide-in." The animation slides the element into view using a predefined animation effect.
     */
-
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+      {/* Mobile-based Sidebar*/}
       <div className="hidden md:flex h-screen flex-initial">
         <Sidebar user={user && user} />
       </div>
       <div className="flex md:hidden flex-row">
-          <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
-              <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)}/>
-              <Link to="/">
-                <img src={logo} alt="logo" className="w-28" />
-              </Link>
-              <Link to={`user-profile/${user?._id}`}>
-                <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full"/>
-              </Link>
-          </div>
-          {toggleSidebar && (
-              <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-                  <div className="absolute w-full flex justify-end items-center p-2">
-                    <AiFillCloseCircle fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
-                  </div>
-                  <Sidebar closeToggle={setToggleSidebar} user={user && user}/>
-              </div>  
-          )}
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+            <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)}/>
+            <Link to="/">
+              <img src={logo} alt="logo" className="w-28" />
+            </Link>
+            <Link to={`user-profile/${user?._id}`}>
+              <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full"/>
+            </Link>
+        </div>
+        
+        {/* Web-based Sidebar*/}
+        {toggleSidebar && (
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+              <div className="absolute w-full flex justify-end items-center p-2">
+                <AiFillCloseCircle fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
+              </div>
+              <Sidebar closeToggle={setToggleSidebar} user={user && user}/>
+          </div>  
+        )}
       </div>
     </div>
   )
