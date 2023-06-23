c13c4ee33c62ca9fe9d8bd54913bbd36f7afb88f
Divya <divyareddy0205@gmail.com>
2023-06-23 15:27:00 -0700

    Home

--- curioscape_frontend/src/container/Home.jsx
+++ curioscape_frontend/src/container/Home.jsx
@@ -61,9 +61,16 @@ const Home = () => {
                 <img src={logo} alt="logo" className="w-28" />
               </Link>
               <Link to={`user-profile/${user?._id}`}>
-                <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full" />
+                <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full"/>
               </Link>
           </div>
+          {toggleSidebar && (
+              <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+                  <div className="absolute w-full flex justify-end items-center p-2">
+                    <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+                  </div>
+              </div>  
+          )}
       </div>
     </div>
   )
