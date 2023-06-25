189d37db8eddd6530c2632c3e3b94b61b1f6d9aa
Divya <divyareddy0205@gmail.com>
2023-06-24 22:10:57 -0700

    navbar

--- curioscape_frontend/src/components/Navbar.js
+++ curioscape_frontend/src/components/Navbar.js
@@ -8,12 +8,12 @@ const Navbar = ({searchTerm, setSearchTerm, user}) => {
   if(user){
     return (
         <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7">
-            <div className="flex justify-start items-center">
-                <IoMdSearch fontSize={21} className="ml-1"/>
+            <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
+                <IoMdSearch fontSize={21} className="ml-1" />
                 <input
                     type="text"
                     onChange={(e) => setSearchTerm(e.target.value)}
-                    placeholder='search'
+                    placeholder="Search"
                     value={searchTerm}
                     onFocus={() => navigate('/search')}
                     className="p-2 w-full bg-white outline-none"
