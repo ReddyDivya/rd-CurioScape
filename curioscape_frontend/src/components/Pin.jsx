26cd7451d9bc6b55a338dc0b6511f371f8bbf684
Divya <divyareddy0205@gmail.com>
2023-06-25 16:01:58 -0700

    pin

--- curioscape_frontend/src/components/Pin.jsx
+++ curioscape_frontend/src/components/Pin.jsx
@@ -3,7 +3,7 @@ import {urlFor} from '../client';
 
 const Pin = ({pin: {postedBy, image, _id, destination}}) => {
   return (
-    <div>
+    <div className='m-1'>
       <img className="rounded-lg w-full" alt="user-post" src={(urlFor(image).width(250).url())}/>
     </div>
   )
