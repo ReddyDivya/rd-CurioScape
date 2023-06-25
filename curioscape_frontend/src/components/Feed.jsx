fda002d978a03bbd507c3d84f2a2bc64daf92e28
Divya <divyareddy0205@gmail.com>
2023-06-25 12:59:29 -0700

    feed

--- curioscape_frontend/src/components/Feed.jsx
+++ curioscape_frontend/src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 
-import {feedQuery} from "../utils/data";//fetching queries
+import {feedQuery, searchQuery} from "../utils/data";//fetching queries
 import {client} from "../client";
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
@@ -20,11 +20,41 @@ const Feed = () => {
   const [loading, setLoading] = useState(true);//to display spinner
   const {categoryId} = useParams();//object destructuring
 
+  useEffect(() => {
+
+    //only if category exists
+    if(categoryId)
+    {
+      setLoading(true);//show spinner
+      
+      //searching the pins data through categoryId
+      const query = searchQuery(categoryId);
+
+      //fetching pins data from the searched category
+      client.fetch(query).then((data) => {
+        setPins(data);//setting the fetched pins data from the searched category.
+        setLoading(false);//hiding the spinner
+      });
+    }//if
+    else{
+      setLoading(true);//show spinner
+
+      //fetching feed data
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);//setting the fetched pins data from the searched category.
+        setLoading(false);//hide spinner
+      });
+    }//else
+  }, [categoryId]);
+
+  const ideaName = categoryId || 'new'; //new category is created, if categoryId doesn't exists.
+
   if(loading)
     return <Spinner message={`We are adding new ideas to your feed!`} />
+
   return (
     <div>
-      Feed    
+          
     </div>
   )
 }
