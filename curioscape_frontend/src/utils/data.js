98d40bb9912c6430f8fcbb1898989170a9cde0bb
Divya <divyareddy0205@gmail.com>
2023-06-25 18:20:46 -0700

    data

--- curioscape_frontend/src/utils/data.js
+++ curioscape_frontend/src/utils/data.js
@@ -210,3 +210,28 @@ export const pinDetailMorePinQuery  = (pin) => {
   return query;
 };//pinDetailMorePinQuery
 
+export const userCreatedPinsQuery  = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+}; //userCreatedPinsQuery 
+
