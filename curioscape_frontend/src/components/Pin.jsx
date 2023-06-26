be26dbc7b273f16ca1225263f06b6f51bf775fbd
Divya <divyareddy0205@gmail.com>
2023-06-25 17:27:50 -0700

    pin

--- curioscape_frontend/src/components/Pin.jsx
+++ curioscape_frontend/src/components/Pin.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
-import {urlFor} from '../client';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { AiTwotoneDelete } from 'react-icons/ai';
+import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
+
+import { client, urlFor } from '../client';
 
 const Pin = ({pin: {postedBy, image, _id, destination}}) => {
   return (
