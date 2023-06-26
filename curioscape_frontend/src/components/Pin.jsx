b74fdfbd4b61e9b2634c98d144a082477532e711
Divya <divyareddy0205@gmail.com>
2023-06-26 16:18:16 -0700

    pin

--- curioscape_frontend/src/components/Pin.jsx
+++ curioscape_frontend/src/components/Pin.jsx
@@ -122,8 +122,16 @@ const Pin = ({pin}) => {
           </div>
         )}
     </div>
-    <Link to={`/user-profile/${postedBy?._id}`}>
-        
+
+    {/* Display user-profile pic | name and navigate to userprofile component */}
+    <Link to={`/user-profile/${postedBy?._id}`}
+      className='flex gap-2 mt-2 items-center'>
+        <img
+          className="w-8 h-8 rounded-full object-cover"
+          src={postedBy?.image}
+          alt="user-profile"
+        />
+        <p className="font-semibold capitalize">{postedBy?.userName}</p>
     </Link>
   </div>
   )
