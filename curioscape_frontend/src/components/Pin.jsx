9b0750e7e1183e4f83c4d1375c4b1287a843d711
Divya <divyareddy0205@gmail.com>
2023-06-25 17:44:59 -0700

    pin

--- curioscape_frontend/src/components/Pin.jsx
+++ curioscape_frontend/src/components/Pin.jsx
@@ -30,6 +30,28 @@ const Pin = ({pin}) => {
       {
         image && (<img className="rounded-lg w-full" alt="user-post" src={(urlFor(image).width(250).url())}/>)
       }
+
+      {
+        postHovered && ( 
+          <div className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
+            style={{ height: '100%' }}>
+              <div className="flex items-center justify-between">
+                <div className="flex gap-2">
+                    <a
+                      href={`${image?.asset?.url}?dl=`}
+                      download
+                      onClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                      className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                    >
+                      <MdDownloadForOffline />
+                    </a>
+                </div>
+              </div>  
+          </div>
+        )
+      }
     </div>  
       
     </div>
