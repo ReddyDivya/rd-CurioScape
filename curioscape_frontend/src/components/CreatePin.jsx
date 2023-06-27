44849483fc308915c527866ee398a9bbd6c9cc53
Divya <divyareddy0205@gmail.com>
2023-06-27 14:28:29 -0700

    createPin

--- curioscape_frontend/src/components/CreatePin.jsx
+++ curioscape_frontend/src/components/CreatePin.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import { MdDelete } from 'react-icons/md';
+
+import { categories } from '../utils/data';
+import { client } from '../client';
+import Spinner from './Spinner';
 
 const CreatePin = () => {
+
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [destination, setDestination] = useState();
+  const [fields, setFields] = useState();
+  const [category, setCategory] = useState();
+  const [imageAsset, setImageAsset] = useState();
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
   return (
-    <div>CreatePin</div>
+    <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
+        {
+          fields && (
+            <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in ">Please add all fields.</p>
+          )
+        }
+      <div className=" flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5  w-full">
+        <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
+          <div className=" flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
+              {
+                loading && ( <Spinner />)
+              }
+          </div>
+        </div>  
+      </div>  
+    </div>
   )
 }
 
