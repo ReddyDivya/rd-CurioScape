9350b96f03fd585631f97635a5f0be21167c4a91
Divya <divyareddy0205@gmail.com>
2023-06-27 18:45:48 -0700

    pindetails

--- curioscape_frontend/src/components/Pin.jsx
+++ curioscape_frontend/src/components/Pin.jsx
@@ -66,7 +66,7 @@ const Pin = ({pin}) => {
       <div className='relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out'
       onMouseEnter={() => setPostHovered(true)}
       onMouseLeave={() => setPostHovered(false)}
-      onClick={() => navigate(`/pin-detail/${_id}`)}
+      onClick={() => navigate(`/pin-details/${_id}`)}
     >
       {
         image && (<img className="rounded-lg w-full" alt="user-post" src={(urlFor(image).width(250).url())}/>)
