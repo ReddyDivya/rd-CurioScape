612dd1133cfcfd207a45f3b332199e036fd10e37
Divya <divyareddy0205@gmail.com>
2023-06-28 12:56:00 -0700

    logout

--- curioscape_frontend/src/components/Search.jsx
+++ curioscape_frontend/src/components/Search.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div>
+      {loading && <Spinner message="Searching pins" />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {
+        pins?.length === 0 && searchTerm !== '' && !loading && 
+        (
+          <div className="mt-10 text-center text-xl ">No Pins Found!</div>
+        )
+      }
+    </div>
   )
 }
 
