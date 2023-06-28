7f32f3fe7affb76c6890e7b66fd9cdb2ea422fc4
Divya <divyareddy0205@gmail.com>
2023-06-27 17:58:51 -0700

    pindetails

--- curioscape_frontend/src/components/PinDetails.jsx
+++ curioscape_frontend/src/components/PinDetails.jsx
@@ -5,15 +5,34 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { client, urlFor } from '../client';
 import MasonryLayout from './MasonryLayout';
-import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';//fetching the query
 import Spinner from './Spinner';
 
 const PinDetails = ({user}) => {
-  const { pinId } = useParams();
+  const { pinId } = useParams();//param
+
   const [pins, setPins] = useState();
   const [pinDetail, setPinDetail] = useState();
   const [comment, setComment] = useState('');
   const [addingComment, setAddingComment] = useState(false);
+
+  //fetch pin details
+  const fetchPinDetails = () => {
+    const query = pinDetailQuery(pinId);
+
+    if(query)
+    {
+      client.fetch(`${query}`)
+      .then((data) => {
+        setPinDetail(data[0]);//set pin details
+      })
+    }//if
+  };//fetchPinDetails 
+
+  //add comment
+  const addComment = () => {
+
+  };//addComment
 
   return (
     <>
@@ -38,11 +57,69 @@ const PinDetails = ({user}) => {
                     <MdDownloadForOffline />
                   </a>
                 </div>
-              </div>  
+                <a href={pinDetail.destination} target="_blank" rel="noreferrer">
+                  {pinDetail.destination?.slice(8)}
+                </a>
+              </div>
+              <div>
+                <h1 className="text-4xl font-bold break-words mt-3">
+                  {pinDetail.title}
+                </h1>
+                <p className="mt-3">{pinDetail.about}</p>
+              </div>
+              <Link to={`/user-profile/${pinDetail?.postedBy._id}`} className="flex gap-2 mt-5 items-center bg-white rounded-lg ">
+                <img src={pinDetail?.postedBy.image} className="w-10 h-10 rounded-full" alt="user-profile" />
+                <p className="font-bold">{pinDetail?.postedBy.userName}</p>
+              </Link>
+              <h2 className="mt-5 text-2xl">Comments</h2>
+              <div className="max-h-370 overflow-y-auto">
+                {pinDetail?.comments?.map((item) => (
+                  <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={item.comment}>
+                    <img
+                      src={item.postedBy?.image}
+                      className="w-10 h-10 rounded-full cursor-pointer"
+                      alt="user-profile"
+                    />
+                    <div className="flex flex-col">
+                      <p className="font-bold">{item.postedBy?.userName}</p>
+                      <p>{item.comment}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="flex flex-wrap mt-6 gap-3">
+                <Link to={`/user-profile/${user._id}`}>
+                  <img src={user.image} className="w-10 h-10 rounded-full cursor-pointer" alt="user-profile" />
+                </Link>
+                <input
+                  className=" flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+                  type="text"
+                  placeholder="Add a comment"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                />
+                <button
+                  type="button"
+                  className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+                  onClick={addComment}
+                >
+                  {addingComment ? 'Doing...' : 'Done'}
+                </button>
+              </div>
             </div>
-          </div>  
+          </div>
         )
       }
+      {pins?.length > 0 && (
+        <h2 className="text-center font-bold text-2xl mt-8 mb-4">
+          More like this
+        </h2>
+      )}
+      {pins ? (
+        <MasonryLayout pins={pins} />
+      ) : (
+        <Spinner message="Loading more pins" />
+      )}
     </>
   )
 }
