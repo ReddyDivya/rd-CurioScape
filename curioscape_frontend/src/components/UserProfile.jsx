3852a50b83a2ac9634c557edd47d6887e4c1e5f4
Divya <divyareddy0205@gmail.com>
2023-06-27 19:02:02 -0700

    user-profiles

--- curioscape_frontend/src/components/UserProfile.jsx
+++ curioscape_frontend/src/components/UserProfile.jsx
@@ -16,6 +16,8 @@ const notActiveBtnStyles = 'bg-primary mr-4 text-black font-bold p-2 rounded-ful
 
 const UserProfile = () => {
 
+  const { userId } = useParams();//params
+
   const [user, setUser] = useState();//user
   const [pins, setPins] = useState();//for showing pins
   const [text, setText] = useState('Created');
@@ -24,7 +26,49 @@ const UserProfile = () => {
   //navigate
   const navigate = useNavigate();
 
-  const { userId } = useParams();//params
+  //fetching user info from localstorage
+  const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+  //fetching user info using userquery
+  useEffect(() => {
+    const query = userQuery(userId);
+
+    client.fetch(query)
+    .then((data) => {
+      setUser(data[0]);//setting user info
+    })
+  }, [userId]);
+
+  useEffect(() => {
+
+    //fetching user created pins
+    if (text === 'Created') 
+    {
+      const createdPinsQuery = userCreatedPinsQuery(userId);
+
+      client.fetch(createdPinsQuery).then((data) => {
+        setPins(data);
+      });
+    }
+    else //fetching user saved pins
+    {
+      const savedPinsQuery = userSavedPinsQuery(userId);
+
+      client.fetch(savedPinsQuery).then((data) => {
+        setPins(data);
+      });
+    }
+  }, [text, userId]);
+
+  //logout
+  const logout = () => {
+    localStorage.clear();
+
+    navigate('/login');//navigate to login
+  };//logout
+
+  //displaying 'loading message'
+  if (!user) return <Spinner message="Loading profile" />;
 
   return (
     <div>UserProfile</div>
