ea5936eaadf0cc6007ebb12d37d062e83f1f58b3
Divya <divyareddy0205@gmail.com>
2023-06-27 18:48:41 -0700

    user-profiles

--- curioscape_frontend/src/components/UserProfile.jsx
+++ curioscape_frontend/src/components/UserProfile.jsx
@@ -1,4 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useParams, useNavigate } from 'react-router-dom';
+import { GoogleLogout } from 'react-google-login';
+
+import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../utils/data';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+//active button styles
+const activeBtnStyles = 'bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none';
+
+//not active button styles
+const notActiveBtnStyles = 'bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none';
 
 const UserProfile = () => {
   return (
