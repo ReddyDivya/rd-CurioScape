62b64c67622f02aebe6bbbb6ddf2398a96363912
Divya <divyareddy0205@gmail.com>
2023-06-28 12:56:53 -0700

    search

--- curioscape_frontend/src/components/Search.jsx
+++ curioscape_frontend/src/components/Search.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import MasonryLayout from './MasonryLayout';
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import Spinner from './Spinner';
 
 const Search = () => {
+  const [pins, setPins] = useState(); //show pins
+  const [loading, setLoading] = useState(false);//show spinner
+
   return (
     <div>
       {loading && <Spinner message="Searching pins" />}
