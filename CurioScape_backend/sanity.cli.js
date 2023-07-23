094d7612c83c4a0c046643fb6bc66a4215ff1b31
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-07-23 09:06:41 +0530

    Update sanity.cli.js

--- CurioScape_backend/sanity.cli.js
+++ CurioScape_backend/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'x8qy9fhj',
-    dataset: 'production'
+    projectId: '',
+    dataset: ''
   }
 })
